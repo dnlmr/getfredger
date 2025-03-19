351926e3de25a5403aaa0a9e454d881fcf3f4435
Daniel Meier <daniel@eck115.de>
2025-03-19 15:49:08 +0100

    Enhance team invite functionality; implement authorization checks, unique email validation, and update TeamInvite model relationships

--- app/Http/Requests/TeamInvitesStoreRequest.php
+++ app/Http/Requests/TeamInvitesStoreRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class TeamInvitesStoreRequest extends FormRequest
@@ -11,7 +13,7 @@ class TeamInvitesStoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return $this->user()->can('inviteToTeam', $this->team);
     }
 
     /**
@@ -27,7 +29,10 @@ class TeamInvitesStoreRequest extends FormRequest
                 'string',
                 'lowercase',
                 'email',
-                'max:255'],
+                'max:255',
+                Rule::unique('team_invites')
+                    ->where('team_id', $this->user()->currentTeam->id)
+            ],
         ];
     }
 }
--- tests/Feature/Controllers/TeamInviteControllerTest.php
+++ tests/Feature/Controllers/TeamInviteControllerTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Team;
 use App\Models\User;
+use App\Models\TeamInvite;
 use function Pest\Laravel\actingAs;
+use App\Http\Middleware\TeamsPermission;
 use function Pest\Laravel\assertDatabaseHas;
 
 afterEach(function () {
@@ -34,4 +37,53 @@ it('requires an email address', function() {
             'email' => ''
         ])
         ->assertSessionHasErrors('email');
+});
+
+it('fails to create an invite if email is already used on team', function() {
+    $user = User::factory()->create();
+
+    TeamInvite::factory()->create([
+        'team_id' => $user->currentTeam->id,
+        'email' => $email = 'invite@example.com'
+    ]);
+
+    actingAs($user)
+        ->post(route('team.invites.store', $user->currentTeam), [
+            'email' => $email
+        ])
+        ->assertInvalid();
+});
+
+
+it('creates invite if email already invited to another team', function () {
+    $user = User::factory()->create();
+
+    TeamInvite::factory()
+        ->for(Team::factory())
+        ->create([
+            'email' => $email = 'invite@example.com'
+        ]);
+
+    actingAs($user)
+        ->post(route('team.invites.store', $user->currentTeam), [
+            'email' => $email
+        ])
+        ->assertValid();
+});
+
+it('fails to send invite without permission', function () {
+    $user = User::factory()->create();
+
+    $user->teams()->attach(
+        $anotherTeam = Team::factory()->create()
+    );
+
+    setPermissionsTeamId($anotherTeam->id);
+
+    actingAs($user)
+        ->withoutMiddleware(TeamsPermission::class)
+        ->post(route('team.invites.store', $anotherTeam), [
+            'email' => 'invite@example.com'
+        ])
+        ->assertForbidden();
 });
