241f500b403292b802b75f49f602d13e4122cb71
Daniel Meier <daniel@eck115.de>
2025-04-01 14:06:09 +0200

    Enhance team invitation email with additional details about collaboration and features

--- resources/views/emails/team_invitation.blade.php
+++ resources/views/emails/team_invitation.blade.php
@@ -1,9 +1,20 @@
 <x-mail::message>
 # You have been invited to join {{ $teamInvite->team->name }}!
 
+You have been invited to collaborate with the team "{{ $teamInvite->team->name }}". By accepting this invitation, you will gain access to the team.
+
+{{ config('app.name') }} helps teams manage and share invoices efficiently. As a member of this team, you'll be able to:
+
+- Upload and view invoice images
+- Organize invoices by categories or projects
+- Share financial documents with team members
+- Track and manage payment records together
+
 <x-mail::button :url="$url">
 Accept Invitation
 </x-mail::button>
+
+This invitation will expire in 3 days. If you did not expect to receive this invitation, you can safely ignore this email.
 
 Thanks,<br>
 {{ config('app.name') }}
