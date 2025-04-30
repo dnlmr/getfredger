f356be733c7c0d3efd78c03063a4db65a6166991
Daniel Meier <daniel@eck115.de>
2025-04-30 08:07:46 +0200

    Add sidebar state handling and update related components

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->encryptCookies(except: ['appearance']);
+        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
             TeamsPermission::class,
