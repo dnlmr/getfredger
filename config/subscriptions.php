8abf269f79e597b342183d1f8a06bf4fcd8f5519
Daniel Meier <daniel@eck115.de>
2025-08-08 18:30:56 +0200

    Refactor subscription plans to include 'Fredger Pro' naming and adjust max monthly credits

--- config/subscriptions.php
+++ config/subscriptions.php
@@ -3,16 +3,22 @@
 return [
     'plans' => [
         'monthly' => [
-            'name' => 'Monthly',
-            'plan_id' => env('FREDGER_MONTHLY_ID'),
+            'name' => 'Fredger Pro Monthly',
+            'plan_id' => env('FREDGER_PRO_MONTHLY_ID'),
             'price' => '900',
-            'max_monthly_credits' => 200,
+            'max_monthly_credits' => 100,
+        ],
+        'yearly' => [
+            'name' => 'Fredger Pro Yearly',
+            'plan_id' => env('FREDGER_PRO_YEARLY_ID'),
+            'price' => '9900',
+            'max_monthly_credits' => 100,
         ],
         'yearly_believer' => [
-            'name' => 'Yearly Believer',
-            'plan_id' => env('FREDGER_BELIEVER_YEARLY_ID'),
+            'name' => 'Fredger Pro Believer',
+            'plan_id' => env('FREDGER_PRO_BELIEVER_YEARLY_ID'),
             'price' => '4900',
-            'max_monthly_credits' => 200,
+            'max_monthly_credits' => 100,
         ],
     ],
 ];
